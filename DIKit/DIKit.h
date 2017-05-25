193ea0fe9272c69d81f48f1e69b4ae58cecfe91c
Yosuke Ishikawa <y@ishkawa.org>
2017-05-25 16:06:23 +0900

    Add Xcode project

---
+++ DIKit/DIKit.h
@@ -0,0 +1,12 @@
+//
+//  DIKit.h
+//  DIKit
+//
+//  Created by ishkawa on 2017/05/25.
+//  Copyright © 2017 ishkawa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+FOUNDATION_EXPORT double DIKitVersionNumber;
+FOUNDATION_EXPORT const unsigned char DIKitVersionString[];
---
+++ DIKitTests/DIKitTests.swift
@@ -0,0 +1,15 @@
+//
+//  DIKitTests.swift
+//  DIKitTests
+//
+//  Created by ishkawa on 2017/05/25.
+//  Copyright © 2017 ishkawa. All rights reserved.
+//
+
+import XCTest
+import DIKit
+
+class DIKitTests: XCTestCase {
+    func testExample() {
+    }
+}
