dc83682616b2bc75ee080e8ea774acbecb5374df
Yosuke Ishikawa <y@ishkawa.org>
2017-09-15 22:53:36 +0900

    Delete old graph types

--- Sources/dikitgen/main.swift
+++ Sources/dikitgen/main.swift
@@ -33,32 +33,6 @@ protocol Resolver: DIKit.Resolver {
 
 let file = File(path: #file)!
 let structure = Structure(file: file)
-let types = structure.substructures.flatMap(Type.init)
-let resolverNameRequiresModuleName = !types.filter({ $0.name == "Resolver" }).isEmpty
-let resolverName = resolverNameRequiresModuleName ? "DIKit.Resolver" : "Resolver"
-let resolverFunctions = Array(types
-    .filter { $0.kind == .protocol }
-    .filter { $0.inheritedTypes.contains(resolverName) }
-    .map { $0.functions }
-    .joined())
-
-let providableTypeNames = resolverFunctions
-    .filter { !$0.isInitializer && !$0.isStatic && $0.name.hasPrefix("provide") }
-    .flatMap { provider -> String? in
-        let start = file.contents.index(file.contents.startIndex, offsetBy: 449)
-        let end = file.contents.index(start, offsetBy: 20)
-        let components = file.contents[start..<end].components(separatedBy: "->")
-        guard let typeName = components.last?.trimmingCharacters(in: .whitespaces) else {
-            return nil
-        }
-
-        return typeName
-    }
-
-let providables = types.filter { providableTypeNames.contains($0.name) }
-let injectables = types.filter { $0.isInjectable }
-let graph = try! Graph(injectables: injectables, providables: providables)
-print(graph.generateCode().content)
 
 let template = Template(templateString: """
 //
