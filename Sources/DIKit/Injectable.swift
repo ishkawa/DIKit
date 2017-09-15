8bf983ed8a467a6deeb46df374e842d6915790a5
Yosuke Ishikawa <y@ishkawa.org>
2017-09-15 21:26:46 +0900

    Add injectable protocols

--- Sources/DIKit/Injectable.swift
+++ Sources/DIKit/Injectable.swift
@@ -1,5 +1,30 @@
-public protocol Injectable {
+//
+//  Protocols.swift
+//  DIKit
+//
+//  Created by Yosuke Ishikawa on 2017/09/15.
+//
+
+public protocol Injectable {}
+
+public protocol InitializerInjectable {
     associatedtype Dependency
+    init(dependency: Dependency)
+}
+
+public protocol FactoryMethodInjectable {
+    associatedtype Dependency
+    static func makeInstance(dependency: Dependency) -> Self
+}
+
+public protocol PropertyInjectable {
+    associatedtype Dependency
+    var dependency: Dependency! { get set }
+}
+
+public protocol MethodInjectable {
+    associatedtype Dependency
+    func inject(dependency: Dependency)
 }
 
 public protocol Resolver {}
