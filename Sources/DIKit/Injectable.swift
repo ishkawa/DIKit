9947a7146f0764d40924ac83c936aa1e8b85c3fa
Yosuke Ishikawa <y@ishkawa.org>
2017-09-16 22:57:01 +0900

    Delete unused protocols

--- Sources/DIKit/Injectable.swift
+++ Sources/DIKit/Injectable.swift
@@ -5,24 +5,7 @@
 //  Created by Yosuke Ishikawa on 2017/09/15.
 //
 
-public protocol Injectable {}
-
-public protocol InitializerInjectable {
+public protocol Injectable {
     associatedtype Dependency
     init(dependency: Dependency)
-}
-
-public protocol FactoryMethodInjectable {
-    associatedtype Dependency
-    static func makeInstance(dependency: Dependency) -> Self
-}
-
-public protocol PropertyInjectable {
-    associatedtype Dependency
-    var dependency: Dependency! { get set }
-}
-
-public protocol MethodInjectable {
-    associatedtype Dependency
-    func inject(dependency: Dependency)
 }
