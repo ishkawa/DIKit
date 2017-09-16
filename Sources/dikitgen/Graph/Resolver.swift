23f02411146d492df8e2bede37d501b7a93bb9b2
Yosuke Ishikawa <y@ishkawa.org>
2017-09-16 19:23:11 +0900

    Resolve dependency graph

--- Sources/dikitgen/Graph/Resolver.swift
+++ Sources/dikitgen/Graph/Resolver.swift
@@ -16,7 +16,32 @@ struct Resolver {
             return nil
         }
 
+        var unresolvedNodes = injectableTypeNodes + type.functions.flatMap(Node.init(providerMethod:))
+        var resolvedNodes = [] as [Node]
+
+        while !unresolvedNodes.isEmpty {
+            var resolved = false
+            for (index, unresolvedNode) in unresolvedNodes.enumerated() {
+                let resolvable = unresolvedNode.dependencyTypeNames
+                    .reduce(true) { result, unresolvedNode in
+                        return result && resolvedNodes.contains { $0.typeName == unresolvedNode }
+                    }
+
+                if resolvable {
+                    unresolvedNodes.remove(at: index)
+                    resolvedNodes.append(unresolvedNode)
+                    resolved = true
+                    break
+                }
+            }
+
+            if !resolved {
+                // TODO: Throw error
+                return nil
+            }
+        }
+
+        nodes = resolvedNodes
         name = type.name
-        nodes = injectableTypeNodes + type.functions.flatMap(Node.init(providerMethod:))
     }
 }
