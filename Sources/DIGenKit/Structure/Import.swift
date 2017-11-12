306216108fc53259246b3ce94b2e0d90d6ecbd9d
Yosuke Ishikawa <y@ishkawa.org>
2017-11-13 01:43:37 +0900

    Add structure for recognizing module imports

---
+++ Sources/DIGenKit/Structure/Import.swift
@@ -0,0 +1,49 @@
+//
+//  Import.swift
+//  DIGenKit
+//
+//  Created by Yosuke Ishikawa on 2017/11/13.
+//
+
+import Foundation
+import SourceKittenFramework
+
+struct Import {
+    let moduleName: String
+
+    static func imports(from file: File) -> [Import] {
+        let syntaxMap = SyntaxMap(file: file)
+        let importTokenIndices = syntaxMap.tokens.enumerated()
+            .flatMap { index, token -> Int? in
+                guard token.type == "source.lang.swift.syntaxtype.keyword" else {
+                    return nil
+                }
+
+                let view = file.contents.utf8
+                let startIndex = view.index(view.startIndex, offsetBy: Int(token.offset))
+                let endIndex = view.index(startIndex, offsetBy: Int(token.length))
+                let value = String(view[startIndex..<endIndex])!
+                return value == "import" ? index : nil
+            }
+
+        let importedModuleNames = importTokenIndices
+            .flatMap { index -> String? in
+                let identifierIndex = index + 1
+                guard identifierIndex < syntaxMap.tokens.count else {
+                    return nil
+                }
+
+                let token = syntaxMap.tokens[identifierIndex]
+                guard token.type == "source.lang.swift.syntaxtype.identifier" else {
+                    return nil
+                }
+
+                let view = file.contents.utf8
+                let startIndex = view.index(view.startIndex, offsetBy: Int(token.offset))
+                let endIndex = view.index(startIndex, offsetBy: Int(token.length))
+                return String(view[startIndex..<endIndex])!
+            }
+
+        return importedModuleNames.map(Import.init(moduleName:))
+    }
+}
---
+++ Tests/DIGenKitTests/Structure/ImportTests.swift
@@ -0,0 +1,37 @@
+//
+//  ImportTests.swift
+//  DIGenKitTests
+//
+//  Created by Yosuke Ishikawa on 2017/11/13.
+//
+
+import Foundation
+import XCTest
+import SourceKittenFramework
+
+@testable import DIGenKit
+
+final class ImportTests: XCTestCase {
+    func test() {
+        let code = """
+            import Foundation
+            import DIKit
+
+            struct Test: A, B {
+                typealias C = D
+                struct E {}
+                var someProperty: F
+                func someFunction() -> G {}
+            }
+
+            import Foundation
+            """
+
+        let file = File(contents: code)
+        let imports = Import.imports(from: file)
+        let moduleNames = imports.map { $0.moduleName }
+
+        XCTAssertEqual(moduleNames, ["Foundation", "DIKit", "Foundation"])
+    }
+}
+
