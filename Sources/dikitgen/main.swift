470347735c6b2b9747b91f69b029f0b00eb1b7a8
takasek <takassekiyoshi@gmail.com>
2018-02-11 19:30:21 +0900

    Implement a mode to exclude directories defined with arguments

--- Sources/dikitgen/main.swift
+++ Sources/dikitgen/main.swift
@@ -1,21 +1,47 @@
 import Foundation
 import DIGenKit
 
-guard CommandLine.arguments.count == 2 else {
+enum Mode {
+    case version
+    case generate(path: String, excluding: [String])
+}
+struct InvalidArgumentsError: Error {}
+func mode(from arguments: [String]) throws -> Mode {
+    switch arguments.count {
+    case 2 where arguments[1] == "--version":
+        return .version
+    case 2:
+        return .generate(path: arguments[1], excluding: [])
+    case 3..<Int.max:
+        let options = arguments.dropFirst(2)
+        guard
+            options.first == "--exclude" && options.count >= 2
+            else { throw InvalidArgumentsError() }
+        return .generate(path: arguments[1], excluding: options.dropFirst().map { $0 })
+    case _:
+        throw InvalidArgumentsError()
+    }
+}
+
+let path: String
+let exclusions: [String]
+do {
+    switch try mode(from: CommandLine.arguments) {
+    case .version:
+        print(Version.current)
+        exit(0)
+    case .generate(let p, let xs):
+        path = p
+        exclusions = xs
+    }
+} catch {
     print("error: invalid arguments", to: &standardError)
-    print("usage: dikitgen <path to source code directory>", to: &standardError)
+    print("usage: dikitgen <path to source code directory> [--exclude <subpaths to exclude>...]", to: &standardError)
     exit(1)
 }
 
-if CommandLine.arguments[1] == "--version" {
-    print(Version.current)
-    exit(0)
-}
-
-let path = CommandLine.arguments[1]
-
 do {
-    let generator = try CodeGenerator(path: path)
+    let generator = try CodeGenerator(path: path, excluding: exclusions)
     print(try generator.generate())
 } catch let anyError {
     guard
