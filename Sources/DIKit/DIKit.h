9f861a45395fe6a8e0f6e0d5d7c2eae68ae3d564
Yosuke Ishikawa <y@ishkawa.org>
2018-10-30 03:07:18 +0900

    Support multiple platforms

--- Sources/DIKit/DIKit.h
+++ Sources/DIKit/DIKit.h
@@ -6,14 +6,8 @@
 //  Copyright © 2018 Yosuke Ishikawa. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
+#import <Foundation/Foundation.h>
 
-//! Project version number for DIKit.
 FOUNDATION_EXPORT double DIKitVersionNumber;
-
-//! Project version string for DIKit.
 FOUNDATION_EXPORT const unsigned char DIKitVersionString[];
-
-// In this header, you should import all the public headers of your framework using statements like #import <DIKit/PublicHeader.h>
-
 
