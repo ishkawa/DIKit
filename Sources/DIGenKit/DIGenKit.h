139b2ae008f7ff31593bb5b9e6b10251c88afca9
Yosuke Ishikawa <y@ishkawa.org>
2018-10-30 00:44:40 +0900

    [ci skip][WIP] Migrate to xcodeproj generated by Xcode

---
+++ Sources/DIGenKit/DIGenKit.h
@@ -0,0 +1,19 @@
+//
+//  DIGenKit.h
+//  DIGenKit
+//
+//  Created by Yosuke Ishikawa on 2018/10/30.
+//  Copyright © 2018 Yosuke Ishikawa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DIGenKit.
+FOUNDATION_EXPORT double DIGenKitVersionNumber;
+
+//! Project version string for DIGenKit.
+FOUNDATION_EXPORT const unsigned char DIGenKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DIGenKit/PublicHeader.h>
+
+
---
+++ Sources/DIKit/DIKit.h
@@ -0,0 +1,19 @@
+//
+//  DIKit.h
+//  DIKit
+//
+//  Created by Yosuke Ishikawa on 2018/10/30.
+//  Copyright © 2018 Yosuke Ishikawa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DIKit.
+FOUNDATION_EXPORT double DIKitVersionNumber;
+
+//! Project version string for DIKit.
+FOUNDATION_EXPORT const unsigned char DIKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DIKit/PublicHeader.h>
+
+
