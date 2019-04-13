c3dd5caa8c8488bd5174126cb3af69280ad60069
Yosuke Ishikawa <y@ishkawa.org>
2019-04-13 17:51:46 +0900

    Update Pcakage.swift

--- Package.swift
+++ Package.swift
@@ -1,20 +1,24 @@
-// swift-tools-version:4.0
+// swift-tools-version:5.0
 import PackageDescription
 
 let package = Package(
   name: "DIKit",
+  platforms: [
+      .macOS(.v10_11), .iOS(.v9), .tvOS(.v9), .watchOS(.v2)
+  ],
   products: [
     .executable(name: "dikitgen", targets: ["dikitgen"]),
     .library(name: "DIKit", targets: ["DIKit"]),
     .library(name: "DIGenKit", targets: ["DIGenKit"])
   ],
   dependencies: [
-    .package(url: "https://github.com/jpsim/SourceKitten.git", from: "0.21.2"),
+    .package(url: "https://github.com/jpsim/SourceKitten.git", from: "0.23.0")
   ],
   targets: [
     .target(name: "DIKit"),
     .target(name: "DIGenKit", dependencies: ["DIKit", "SourceKittenFramework"]),
     .target(name: "dikitgen", dependencies: ["DIGenKit"]),
     .testTarget(name: "DIGenKitTests", dependencies: ["DIGenKit"])
-  ]
+  ],
+  swiftLanguageVersions: [.v5]
 )
